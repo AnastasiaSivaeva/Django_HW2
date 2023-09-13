c72c1b247eccc42d8eb34d794ee2d9ce28253e3f
Anastasia <anastasia.koles@gmail.com>
2023-09-13 19:36:59 +0300

    HW_3

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,33 @@
-import logging
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
 
-logger = logging.getLogger(__name__)
+from myapp.models import Order
+from unicodedata import decimal
+from .models import User, Order, Product
 
 
+def hello(request):
+    return HttpResponse("Hello World from function!")
+
+
+class HelloView(View):
+    def get(self, request):
+        return HttpResponse("Hello World from class!")
+
+
+def my_view(request):
+    context = {"name": "John"}
+    return render(request, "myapp/index.html", context)
+
+
+class TemplIf(TemplateView):
+    template_name = "myapp/templ_if.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = User.objects.get(pk=self.kwargs['id_user'])
+        orders = Order.objects.filter(user=user).all()
+        context['order'] = orders
+        return context
